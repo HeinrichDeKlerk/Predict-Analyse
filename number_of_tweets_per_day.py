2ce7f520e48ccf48fefa3d405bf4fac7baabc0a2
HeinrichDeKlerk <59819239+HeinrichDeKlerk@users.noreply.github.com>
2020-02-18 15:35:58 +0200

    Update docstring on number_of_tweets_per_day

--- number_of_tweets_per_day.py
+++ number_of_tweets_per_day.py
@@ -1,6 +1,9 @@
-### START FUNCTION
 def number_of_tweets_per_day(df):
-    # your code here
+    """
+    This function takes in a pandas dataframe as input argument.
+    It then returns a new dataframe, grouped by day, with the number of tweets for that day.
+    New dataframe has index of Date(datetime object) and column of 'Tweets"
+    """
     i=0
     while i < len(df):
         df['Date'][i] = df['Date'][i][:11]
@@ -8,5 +11,3 @@ def number_of_tweets_per_day(df):
     df['Date'] = pd.to_datetime(df['Date'])
     a = pd.DataFrame(df.groupby('Date').count().loc[:,'Tweets'])
     return a
-
-### END FUNCTION
