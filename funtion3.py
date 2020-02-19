04de6525d254fb52f596ccb8ecc39e4f962bf2c5
HeinrichDeKlerk <59819239+HeinrichDeKlerk@users.noreply.github.com>
2020-02-19 07:06:43 +0200

    Delete duplicate function

--- funtion3.py
+++
@@ -1,9 +0,0 @@
-def date_parser(dates):
-    """
-    Doc strings here
-    """
-    new_dates = []
-    for date in dates:
-        my_dates = date.split()
-        new_dates.append(my_dates[0])
-    return new_dates
