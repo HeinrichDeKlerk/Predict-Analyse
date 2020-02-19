e8c11dab34acf05491b97ef9c1d8bc86d998c0ca
HeinrichDeKlerk <59819239+HeinrichDeKlerk@users.noreply.github.com>
2020-02-19 08:37:31 +0200

    Update five_num_summary.py

--- five_num_summary.py
+++ five_num_summary.py
@@ -12,11 +12,14 @@ def five_num_summary(items):
 
     """
     
-    a=np.max(items)
-    s=np.median(items)
-    d=np.min(items)
-    f=np.quantile(items, .25)
-    g=np.quantile(items, .75)
-    
-    return dict(max=round(a,2), median=round(s,2),min =round(d,2), q1=round(f,2), q3=round(g,2))
+    for num in items:
+        s_list = sorted(items)
+        n_max = np.max(s_list)
+        n_med = np.median(s_list)       
+        n_q1 = np.percentile(s_list, 25)
+        n_min = np.min(s_list)
+        n_q3 = np.percentile(s_list, 75)
+        
+        D = { "max":n_max, "median":n_med, "min":n_min, "q1":n_q1, "q3":n_q3}
+    return D
 
